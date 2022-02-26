2429d8ed1d5460dcbe9a78124603a43c1f0ef290
Daniel Bong <bongster88@gmail.com>
2022-02-26 15:26:17 +0800

    closes #3 configure swagger docs

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: all
+
+check-install:
+	which swagger || GO11MOBULE=off go get -u github.com/go-swagger/go-swagger/cmd/swagger
+
+docs: check-install
+	GO11MOBULE=off swagger generate markdown -f ./src/swagger.yml --output ./docs/docs.md
+
+swagger: check-install
+	GO11MOBULE=off swagger generate spec -w ./src -o ./src/swagger.yml --scan-models
--- src/api/main.go
+++ src/api/main.go
@@ -1,6 +1,62 @@
+// Package classification Payment API.
+//
+// Documentation for Payment API
+//
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+//
+// Consumes:
+// - application/json
+// Produces:
+// - application/json
+// swagger:meta
 package api
 
 import "github.com/gin-gonic/gin"
+
+// swagger:response validationError
+type ValidationError struct {
+	// The error message
+	// in: body
+	Body struct {
+		// The validation message
+		//
+		// Required: true
+		// Example: Expected type int
+		Message string
+		// An optional field name to which this validation applies
+		FieldName string
+	}
+}
+
+// swagger:response genericError
+type genericError struct {
+	// The error message
+	// in: body
+	Body struct {
+		// The validation message
+		//
+		// Required: true
+		// Example: Expected type int
+		Message string
+		// An optional field name to which this validation applies
+		Error error
+	}
+}
+
+// swagger:response someResponse
+type someResponse struct {
+	// The error message
+	// in: body
+	Body struct {
+		// The validation message
+		//
+		// Required: true
+		// Example: Expected type int
+		Message string
+	}
+}
 
 func Run() {
 	r := gin.Default()
@@ -9,4 +65,54 @@ func Run() {
 			"message": "pong",
 		})
 	})
+	meRouter := r.Group("/me")
+	// 	swagger:route GET /me me me
+	// 		Responses:
+	// 			default: genericError
+	// 			200: someResponse
+	meRouter.GET("/", func(c *gin.Context) {
+		me := make(map[string]interface{}, 0)
+		c.JSON(200, me)
+	})
+
+	// swagger:route GET /me/balance me meBlanace
+	// 		Responses:
+	// 			default: genericError
+	// 			200: someResponse
+	meRouter.GET("/balance", func(c *gin.Context) {
+		balance := make(map[string]interface{}, 0)
+		c.JSON(200, balance)
+	})
+
+	// swagger:route GET /me/transactions me meTransactions
+	// 		Responses:
+	// 			default: genericError
+	// 			200: someResponse
+	meRouter.GET("/transactions", func(c *gin.Context) {
+		transactions := make([]map[string]interface{}, 0)
+		c.JSON(200, transactions)
+	})
+
+	// User API router
+	userRouter := r.Group("/users")
+	// swagger:route GET /users users listUsers
+	// 		Responses:
+	// 			default: genericError
+	// 			200: someResponse
+	userRouter.GET("/", func(c *gin.Context) {
+		users := make([]map[string]interface{}, 0)
+		c.JSON(200, users)
+	})
+
+	// Transaction router
+	transactionRouter := r.Group("/transactions")
+	// swagger:route GET /transactions transactions listTransactions
+	// 		Responses:
+	// 			default: genericError
+	// 			200: someResponse
+	transactionRouter.GET("/", func(c *gin.Context) {
+		transactions := make([]map[string]interface{}, 0)
+		c.JSON(200, transactions)
+	})
+	r.Run()
 }
